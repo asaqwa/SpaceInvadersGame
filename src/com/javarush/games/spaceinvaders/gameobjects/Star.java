4b97567d2ce08bd771b0775f717e735b9375fe7d
Andrey <bursov@web.de>
2021-12-05 21:47:58 +0100

    4/34

--- src/com/javarush/games/spaceinvaders/gameobjects/Star.java
+++ src/com/javarush/games/spaceinvaders/gameobjects/Star.java
@@ -1,8 +1,14 @@
 package com.javarush.games.spaceinvaders.gameobjects;
 
+import com.javarush.engine.cell.*;
 
 public class Star extends GameObject {
     public Star(double x, double y) {
         super(x, y);
+    }
+    private static final String STAR_SIGN = "\u2606";
+
+    public void draw(Game game) {
+        game.setCellValueEx((int)Math.round(x), (int)Math.round(y),Color.NONE, STAR_SIGN, Color.SNOW, 100);
     }
 }
