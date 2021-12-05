d30e9f1c3a292bbbc0a65b607e28f4e4ca15fcd6
Andrey <bursov@web.de>
2021-12-05 20:31:03 +0100

    2/34

--- src/com/javarush/games/spaceinvaders/SpaceInvadersGame.java
+++ src/com/javarush/games/spaceinvaders/SpaceInvadersGame.java
@@ -3,11 +3,22 @@ package com.javarush.games.spaceinvaders;
 import com.javarush.engine.cell.*;
 
 public class SpaceInvadersGame extends Game {
-    public final int WIDTH = 64;
-    public final int HEIGHT = 64;
+    public static final int WIDTH = 64;
+    public static final int HEIGHT = 64;
     
     @Override
     public void initialize() {
         setScreenSize(WIDTH,HEIGHT);
+        createGame();
+    }
+
+    private void drawScene() {
+        drawField();
+    }
+
+    private void drawField() {}
+
+    private void createGame() {
+        drawScene();
     }
 }
