5c42d7bfd10bb29a9b9bb5b532019de96f3252f4
Andrey <bursov@web.de>
2022-12-09 17:53:26 +0100

    30/34

--- src/com/javarush/games/spaceinvaders/gameobjects/Boss.java
+++ src/com/javarush/games/spaceinvaders/gameobjects/Boss.java
@@ -1,7 +1,8 @@
 package com.javarush.games.spaceinvaders.gameobjects;
 
-import static com.javarush.games.spaceinvaders.ShapeMatrix.BOSS_ANIMATION_FIRST;
-import static com.javarush.games.spaceinvaders.ShapeMatrix.BOSS_ANIMATION_SECOND;
+import com.javarush.games.spaceinvaders.Direction;
+
+import static com.javarush.games.spaceinvaders.ShapeMatrix.*;
 
 public class Boss extends EnemyShip {
     private int frameCount = 0;
@@ -14,5 +15,21 @@ public class Boss extends EnemyShip {
     public void nextFrame() {
         frameCount++;
         if (frameCount%10 == 0 || !isAlive) super.nextFrame();
+    }
+
+    @Override
+    public Bullet fire() {
+        if (!isAlive) return null;
+        if (matrix == BOSS_ANIMATION_FIRST)
+            return new Bullet(x + 6, y + height, Direction.DOWN);
+        else
+            return new Bullet(x, y + height, Direction.DOWN);
+    }
+
+    @Override
+    public void kill() {
+        if (!isAlive) return;
+        isAlive = false;
+        setAnimatedView(KILL_BOSS_ANIMATION_FIRST, KILL_BOSS_ANIMATION_SECOND, KILL_BOSS_ANIMATION_THIRD);
     }
 }
